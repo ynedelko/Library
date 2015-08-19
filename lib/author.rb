bc9d5c38140ae8ec931419ed7dbdc0288f79d045
Yelena Nedelko & Rachael Berecka <student@epicodus.com>
2015-08-19 14:22:11 -0700

    Return author by given id

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,9 @@
 class Author
-  attr_reader(:name)
+  attr_reader(:name, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id).to_i()
   end
 
   define_singleton_method(:all) do
@@ -10,16 +11,27 @@ class Author
     authors =[]
     returned_authors.each() do |author|
       name = author.fetch("name")
-      authors.push(Author.new({:name => name}))
+      id = author.fetch('id').to_i()
+      authors.push(Author.new({:name => name, :id => id}))
     end
     authors
   end
 
   define_method(:==) do |another_author|
-    self.name().==(another_author.name())
+    self.name().==(another_author.name())&self.id().==(another_author.id())
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO authors (name) VALUES ('#{@name}')")
+    result =DB.exec("INSERT INTO authors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+  define_singleton_method(:find) do |id|
+    found_author = nil
+    Author.all().each() do |author|
+      if author.id().==(id)
+        found_author = author
+      end
+    end
+    found_author
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,22 +8,31 @@ describe(Author) do
   end
   describe("#save") do
     it("adds an author to an array of author") do
-      test_author = Author.new({:name => "Bohumil Hrabal"})
+      test_author = Author.new({:name => "Bohumil Hrabal", :id => nil})
       test_author.save()
       expect(Author.all()).to(eq([test_author]))
     end
   end
   describe('#==') do
     it('is the same author if it has the same name and id') do
-      test_author1 = Author.new({:name => "Bohumil Hrabal"})
-      test_author2 = Author.new({:name => "Bohumil Hrabal"})
+      test_author1 = Author.new({:name => "Bohumil Hrabal", :id => nil})
+      test_author2 = Author.new({:name => "Bohumil Hrabal", :id => nil})
       expect(test_author1).to(eq(test_author2))
     end
   end
   describe('#name') do
     it("gives the name of the author") do
-      test_author = Author.new({:name => "Bohumil Hrabal"})
+      test_author = Author.new({:name => "Bohumil Hrabal", :id => nil})
       expect(test_author.name()).to(eq("Bohumil Hrabal"))
+    end
+  end
+  describe('.find') do
+    it('returns a book by the books id number') do
+      test_author = Author.new({:name => "Bohumil Hrabal", :id => nil})
+      test_author.save()
+      test_author2 = Author.new({:name => "Shakespeare", :id => nil})
+      test_author2.save()
+      expect(Author.find(test_author2.id())).to(eq(test_author2))
     end
   end
 end
