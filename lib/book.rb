e55d9f24735e3405e5ff1b2e2d09bfc99fa6aa8e
Yelena Nedelko & Rachael Berecka <student@epicodus.com>
2015-08-19 14:13:08 -0700

    Return book by a given number.

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
-  attr_reader(:title)
+  attr_reader(:title, :id)
 
   define_method(:initialize) do |attributes|
     @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id).to_i()
   end
 
   define_singleton_method(:all) do
@@ -10,16 +11,28 @@ class Book
     books =[]
     returned_books.each() do |book|
       title = book.fetch("title")
-      books.push(Book.new({:title => title}))
+      id = book.fetch('id').to_i()
+      books.push(Book.new({:title => title, :id => id}))
     end
     books
   end
 
   define_method(:==) do |another_book|
-    self.title().==(another_book.title())
+    self.title().==(another_book.title()).&(self.id.==(another_book.id()))
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO books (title) VALUES ('#{@title}')")
+    result = DB.exec("INSERT INTO books (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_singleton_method(:find) do |id|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.id().==(id)
+        found_book = book
+      end
+    end
+    found_book
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,22 +8,31 @@ describe(Book) do
   end
   describe("#save") do
     it("adds a book to an array of books") do
-      test_book = Book.new({:title => "Too Loud a Solitude"})
+      test_book = Book.new({:title => "Too Loud a Solitude", :id => nil})
       test_book.save()
       expect(Book.all()).to(eq([test_book]))
     end
   end
   describe('#==') do
-    it('is the same book if it has the same title and id') do
-      test_book1 = Book.new({:title => "Too Loud a Solitude"})
-      test_book2 = Book.new({:title => "Too Loud a Solitude"})
+    it('is the same book if it has the same title') do
+      test_book1 = Book.new({:title => "Too Loud a Solitude", :id => nil})
+      test_book2 = Book.new({:title => "Too Loud a Solitude", :id => nil})
       expect(test_book1).to(eq(test_book2))
     end
   end
   describe('#title') do
     it("gives the title of the book") do
-      test_book = Book.new({:title => "Too Loud a Solitude"})
+      test_book = Book.new({:title => "Too Loud a Solitude", :id => nil})
       expect(test_book.title()).to(eq("Too Loud a Solitude"))
+    end
+  end
+  describe('.find') do
+    it('returns a book by the books id number') do
+      test_book1 = Book.new({:title => "Too Loud a Solitude", :id => nil})
+      test_book1.save()
+      test_book2 = Book.new({:title => "Things They Carry", :id => nil})
+      test_book2.save()
+      expect(Book.find(test_book2.id())).to(eq(test_book2))
     end
   end
 end
