d320868440548a8d4807f46949ae9ffb734a2c06
Yelena Nedelko & Rachael Berecka <student@epicodus.com>
2015-08-19 14:38:49 -0700

    Return patron by given id.

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,25 +1,38 @@
 class Patron
-  attr_reader(:name)
+  attr_reader(:name, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id).to_i()
   end
 
   define_singleton_method(:all) do
     returned_patrons= DB.exec("SELECT * FROM patrons;")
     patrons =[]
-    returned_patrons.each() do |author|
-      name = author.fetch("name")
-      patrons.push(Patron.new({:name => name}))
+    returned_patrons.each() do |patron|
+      name = patron.fetch("name")
+      id = patron.fetch('id')
+      patrons.push(Patron.new({:name => name, :id => id}))
     end
     patrons
   end
 
   define_method(:==) do |another_patron|
-    self.name().==(another_patron.name())
+    self.name().==(another_patron.name()).&self.id().==(another_patron.id())
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}')")
+    result = DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_singleton_method(:find) do |id|
+    found_patron = nil
+    Patron.all().each() do |patron|
+      if patron.id().==(id)
+        found_patron = patron
+      end
+    end
+    found_patron
   end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -8,22 +8,31 @@ describe(Patron) do
   end
   describe("#save") do
     it("adds an patron to an array of patrons") do
-      test_patron = Patron.new({:name => "Reading Girl"})
+      test_patron = Patron.new({:name => "Reading Girl", :id => nil})
       test_patron.save()
       expect(Patron.all()).to(eq([test_patron]))
     end
   end
   describe('#==') do
     it('is the same patron if it has the same name and id') do
-      test_patron1 = Patron.new({:name => "Reading Girl"})
-      test_patron2 = Patron.new({:name => "Reading Girl"})
+      test_patron1 = Patron.new({:name => "Reading Girl", :id => nil})
+      test_patron2 = Patron.new({:name => "Reading Girl", :id => nil})
       expect(test_patron1).to(eq(test_patron2))
     end
   end
   describe('#name') do
     it("gives the name of the patron") do
-      test_patron = Patron.new({:name => "Reading Girl"})
+      test_patron = Patron.new({:name => "Reading Girl", :id => nil})
       expect(test_patron.name()).to(eq("Reading Girl"))
+    end
+  end
+  describe('.find') do
+    it('returns a patron by the patron id number') do
+      test_patron = Patron.new({:name => "Reading Girl", :id => nil})
+      test_patron.save()
+      test_patron2 = Patron.new({:name => "Reading Boy", :id => nil})
+      test_patron2.save()
+      expect(Patron.find(test_patron2.id())).to(eq(test_patron2))
     end
   end
 end
